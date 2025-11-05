cd318c0fa576eb285a752eb4429201f682ebcb22
IsaacMdzX <isaacmendeztp@gmail.com>
2025-11-04 23:47:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-document.getElementById('download-btn').addEventListener('click', () => {
-  const element = document.getElementById('cv-container');
-  const opt = {
-    margin: 0.4,
-    filename: 'Carlos_Isaac_CV.pdf',
-    image: { type: 'jpeg', quality: 0.98 },
+// Generar PDF con html2pdf.js
+document.getElementById("download-btn").addEventListener("click", () => {
+  const element = document.body;
+  const options = {
+    margin: 0.5,
+    filename: "Carlos_Isaac_CV.pdf",
+    image: { type: "jpeg", quality: 0.98 },
     html2canvas: { scale: 2 },
-    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
   };
-  html2pdf().set(opt).from(element).save();
+  html2pdf().set(options).from(element).save();
 });
