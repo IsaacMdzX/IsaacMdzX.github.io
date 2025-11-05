b1f252c5b4d43ccec9e364ca5c24f29186ead68f
IsaacMdzX <isaacmendeztp@gmail.com>
2025-11-04 22:47:57 -0600

    Implement smooth scrolling for anchor links
    Add smooth scrolling effect for menu links

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Efecto suave al hacer clic en los enlaces del menú
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
 
